a3eaca5f99142bf3efb8c16becb10f24ead9315a
Wouter Verbeek <jwverbeek@gmail.com>
2018-12-02 13:11:14 +0100

    evenuneven

--- Datatransformations/evenuneven.js
+++ Datatransformations/evenuneven.js
@@ -1,8 +1,19 @@
 const numbers = [0, 1, 4, 5, 6, 9]
 
 even = numbers.filter(num=> num%2 ==0)
-uneven = numbers.filter(notnum => notnum%2 !==0)
-
+odd = numbers.filter(notnum => notnum%2 !==0)
 
 console.log(even)
-console.log(uneven)
+console.log(odd)
+
+let object1 =  {
+    even: [] , 
+    odd: []
+}
+object1.even.push(even)
+object1.odd.push(odd)
+console.log(object1.even)
+console.log(object1)
+
+
+//object.even.replace(even)
