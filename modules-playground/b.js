ba1a8f3ec019d038e31afb3f54e17d2f259faddc
Wouter Verbeek <jwverbeek@gmail.com>
2018-12-03 08:36:19 +0100

    b.js

--- modules-playground/b.js
+++ modules-playground/b.js
@@ -1,5 +1,5 @@
 module.exports.myName = 'Andrew'
 module.exports.dateOfBirth = '13-09-1991'
 module.exports.sayHello = function(name){
-    console.log(`Hello ${name}! My name is ${myName}`)
+    console.log(`Hello ${name}! My name is ${module.exports.myName}`)
 }
