46b031fd631caaeac08f58250e92864b66824484
Wouter Verbeek <jwverbeek@gmail.com>
2018-12-17 09:38:51 +0100

    array

--- greetings/spread.js
+++ greetings/spread.js
@@ -1,24 +1,38 @@
-const techStartups = [
+const movies = [
     {
-      name: 'Autheos',
-      industry: 'ecommerce'
+      title: 'The Godfather',
+      rating: 9.2,
+      released: 1972,
+      votes: 100
     },
     {
-      name: 'Bunq',
-      industry: 'finance'
+      title: 'The Shawshank Redemption',
+      rating: 9.2,
+      released: 1994,
+      votes: 101
     },
     {
-      name: 'Ligo',
-      industry: 'legal'
+      title: 'The Dark Knight',
+      rating: 8.9,
+      released: 2008,
+      votes: 200
     },
     {
-      name: 'Florin',
-      industry: 'finance'
+      title: 'Star Wars: Episode IV - A New Hope',
+      rating: 8.6,
+      released: 1977,
+      votes: 999
     }
   ]
   
-  const finTechStartups = techStartups.filter((startup) => {
-    return startup.industry === 'finance'
+  const moviesByYear = movies.sort((a, b) => {
+    return b.released - a.released
   })
   
-  console.log(finTechStartups)
+  console.log(moviesByYear)
+  
+  const moviesByRating = moviesByYear.sort((a, b) => {
+    return b.rating - a.rating
+  })
+  
+ // console.log(moviesByRating)
