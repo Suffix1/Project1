a77cf6491b008d555634b4b98dd0ab3498a37212
Wouter Verbeek <jwverbeek@gmail.com>
2018-12-23 07:29:28 +0100

    doglist initliazation

---
+++ code/react/dogslist/src/components/Dogslist.js
@@ -0,0 +1,12 @@
+import React, { PureComponent } from 'react'
+
+export default class Dogslist extends PureComponent {
+    render() {
+        return (
+            <div className="dogs=list">
+                <h1>Dogs List</h1>
+            </div>
+            
+        )
+    }
+}
