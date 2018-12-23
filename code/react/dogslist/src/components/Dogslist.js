b0a0fed9bdf9bed988f1967f3bdb1fa4535a3995
Wouter Verbeek <jwverbeek@gmail.com>
2018-12-23 08:03:01 +0100

    doglistworking

--- code/react/dogslist/src/components/Dogslist.js
+++ code/react/dogslist/src/components/Dogslist.js
@@ -1,10 +1,32 @@
 import React, { PureComponent } from 'react'
+import * as request from 'superagent'
 
 export default class Dogslist extends PureComponent {
+    state = { dogBreeds: null }
+
+    componentDidMount() {
+        request
+            .get('https://dog.ceo/api/breeds/list/all')
+            .then(response => this.updateBreeds(Object.keys(response.body.message)))
+            .catch(console.error)
+    }
+
+    updateBreeds(breeds) {
+        this.setState({
+            dogBreeds: breeds
+        })
+    }
     render() {
         return (
             <div className="dogs=list">
                 <h1>Dogs List</h1>
+
+                { this.state.dogBreeds === null && 'Loading..' }
+                { <ul>
+                    
+                    { this.state.dogBreeds !== null && this.state.dogBreeds.map(breed => <li key={breed}>{breed}</li>)}
+                    
+                </ul>}
             </div>
             
         )
